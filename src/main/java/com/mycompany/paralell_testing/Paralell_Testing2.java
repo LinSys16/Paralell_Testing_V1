033734aa107df7c21fb205c86f2ec20ccba8a58c
Bga1065 <106782405+Bga1065@users.noreply.github.com>
2022-11-20 01:03:16 -0600

    Commit 2

---
+++ src/main/java/com/mycompany/paralell_testing/Paralell_Testing2.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.paralell_testing;
+
+/**
+ *
+ * @author Lindsay
+ */
+public class Paralell_Testing2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
